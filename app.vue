609efca40575a41ecde539405e150f2de98e52d5
Eraly <81900112+Chambo015@users.noreply.github.com>
2023-04-24 00:23:44 +0600

    init

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <header>
+        <img src="~/assets/img/logo.png" alt="bu_logo">
+    </header>
+    <h2>Era</h2>
+  </main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ // content: [], уже настроен nuxt по умолчанию 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
