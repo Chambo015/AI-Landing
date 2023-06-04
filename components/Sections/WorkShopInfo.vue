624abe18d22b2dea15e15691e33dad5f52519ab9
Ернур <Eroxa2580@mail.ru>
2023-06-04 18:50:14 +0600

    workshopinfo fix

--- components/Sections/WorkShopInfo.vue
+++ components/Sections/WorkShopInfo.vue
@@ -1,8 +1,8 @@
 <template>
     <section class="py-4 md:py-8 2xl:py-16">
         <div class="container flex justify-center">
-            <div class="text-[#AAA9A9] py-14 px-0">
-                <h2 class="text-center uppercase mb-16">
+            <div class="py-14 px-0">
+                <h2 class="text-center mb-8 lg:mb-16">
                     <span>Бесплатный</span> Воркшоп
                 </h2>
 
@@ -23,8 +23,8 @@
                 <div class="flex flex-col w-full sm:w-2/3 md:w-full gap-2 mx-auto md:flex-row justify-between items-center mt-8">
                     <p class="lg:text-3xl text-center text-xl">В это воскресенье в 16:00 </p>
 
-                    <button class="text-white bg-btn-gradient space-x-7 lg:text-xl xl:text-3xl text-lg font-Dewi uppercase lg:py-4 py-2 lg:px-4 xl:px-10 max-lg:w-[90%] before:absolute isolate before:bg-light-btn-gradient relative before:inset-0 before:z-[-1] before:opacity-0 focus:before:opacity-100 hover:before:opacity-100 before:transition-opacity active:scale-95 transition-transform">
-                        <a href="https://api.whatsapp.com/send?phone=77081056283" target="_blank">хочу на воркшоп</a>
+                    <button class="text-white bg-btn-gradient space-x-7 lg:text-xl xl:text-3xl text-lg font-Dewi uppercase lg:py-4 py-2 lg:px-4 xl:px-10 max-lg:w-[70%] before:absolute isolate before:bg-light-btn-gradient relative before:inset-0 before:z-[-1] before:opacity-0 focus:before:opacity-100 hover:before:opacity-100 before:transition-opacity active:scale-95 transition-transform">
+                        <a href="https://api.whatsapp.com/send?phone=77476126014" target="_blank">хочу на воркшоп</a>
                     </button>
 
                     <p class="lg:text-3xl text-center text-xl md:text-right "> в zoom конференции</p>
